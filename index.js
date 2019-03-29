5ff48b91141df4e91a0911fcc88cdf05c57b7db6
Wescley Matos <wescleymatos@gmail.com>
2019-03-29 19:21:02 -0300

    Create image robot

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const robots = {
     input: require('./robots/input.js'),
     text: require('./robots/text.js'),
-    state: require('./robots/state.js')
-    //image: require('./robots/image.js')
+    state: require('./robots/state.js'),
+    image: require('./robots/image.js')
   }
   
   async function start() {
     robots.input()
     await robots.text()
-    //await robots.image()
+    await robots.image()
   
     const content = robots.state.load()
     console.dir(content, { depth: null })
